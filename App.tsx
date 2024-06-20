f6ce8d692859535172337a93c886244e31158ad6
ingarisa <arisa.ing2003@gmail.com>
2024-06-20 12:35:04 +0700

    android

--- App.tsx
+++ App.tsx
@@ -1,13 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import appleHealthKit, { HealthInputOptions } from 'react-native-health';
-import  AppleHealthKit, {HealthKitPermissions} from 'react-native-health';
+import  AppleHealthKit, {HealthKitPermissions, HealthInputOptions, HealthUnit} from 'react-native-health';
+import { Platform } from 'react-native';
+
+import {
+  initialize,
+  requestPermission,
+  readRecords,
+} from 'react-native-health-connect';
+import { Permission } from 'react-native-health-connect/lib/typescript/types';
+import { TimeRangeFilter } from 'react-native-health-connect/lib/typescript/types/base.types';
+
 
 const permissions: HealthKitPermissions = {
   permissions: {
     read : [AppleHealthKit.Constants.Permissions.Steps, AppleHealthKit.Constants.Permissions.DistanceWalkingRunning,
-    AppleHealthKit.Constants.Permissions.SleepAnalysis],
+    AppleHealthKit.Constants.Permissions.SleepAnalysis, AppleHealthKit.Constants.Permissions.ActiveEnergyBurned],
     write: [], 
   },
 };
@@ -29,17 +38,33 @@ export default function App() {
   const [steps, setSteps] = useState(0) ;
   const [distance, setDistance] = useState(0) ;
   const [sleep, setSleep] = useState(0) ;
+  const [active, setActive] = useState(0) ;
 
 
- AppleHealthKit.isAvailable(() => {});
+
+ /* AppleHealthKit.isAvailable(() => {}); */ 
   useEffect(() => {
-    AppleHealthKit.initHealthKit(permissions, (err) => {
+    if (Platform.OS != 'ios') {
+      return ; 
+    }
+    AppleHealthKit.isAvailable((err, isAvaliable) => {
       if (err) {
-        console.log('Error getting permissions');
+        console.log('error checking availability');
         return ; 
       }
-      setHasPermission(true) ; 
-  }) ; 
+      if (!isAvaliable) {
+        console.log('Apple Health NOT AVAILABLE'); 
+        return ; 
+      }
+      AppleHealthKit.initHealthKit(permissions, (err) => {
+        if (err) {
+          console.log('Error getting permissions');
+          return ; 
+        }
+        setHasPermission(true) ; 
+    }) ; 
+    }); 
+
 }, []); 
 
 
@@ -51,7 +76,7 @@ useEffect(() => {
 
   const options: HealthInputOptions = {
     date: new Date().toISOString(),  
-    startDate: new Date().toISOString(),
+    startDate: new Date(2024,0,0).toISOString(),
     /*includeManuallyAdded: false, */
 
   } ; 
@@ -69,14 +94,80 @@ useEffect(() => {
     setDistance(results.value) ; 
   });
 
-/* AppleHealthKit.getSleepSamples(options, (err, results) => {
+  AppleHealthKit.getActiveEnergyBurned(options, (err, results) => {
+    if (err) {
+      console.log('Error getting active energy');
+    }
+    setActive(results[0].value) ;
+    console.log(results) ;
+  });
+
+ /* AppleHealthKit.getSleepSamples(options, (err, results) => {
     if (err) {
       console.log('Error getting sleep samples');
     }
-    setSleep(results.value) ; 
-  }); */
+    setSleep(results[0].value) ; 
+  }); */ 
 
 }, [hasPermissions]); 
+
+// const useHealthData = () => {
+	const [androidPermissions, setAndroidPermissions] = useState<Permission[]>([]);
+	
+
+	useEffect(() => {
+    if (Platform.OS !== 'android') {
+      return;
+    }
+    const init = async () => {
+      // initialize the client
+      const isInitialized = await initialize();
+      if (!isInitialized) {
+        console.log('Failed to initialize Health Connect');
+        return;
+      }
+
+      // request permissions
+      const grantedPermissions = await requestPermission([
+        { accessType: 'read', recordType: 'Steps' },
+        { accessType: 'read', recordType: 'Distance' },
+        { accessType: 'read', recordType: 'FloorsClimbed' },
+      ]);
+
+      setAndroidPermissions(grantedPermissions);
+    };
+
+    init();
+  }, []);
+
+// }
+
+const hasAndroidPermission = (recordType: string) => {
+  return androidPermissions.some((perm) => perm.recordType === recordType);
+};
+
+useEffect(() => {
+  if (!hasAndroidPermission('Steps')) {
+    return;
+  }
+  const getHealthData = async () => {
+    const today = new Date();
+    const timeRangeFilter: TimeRangeFilter = {
+      operator: 'between',
+      startTime: new Date(today.getTime() - 86400000).toISOString(),
+      endTime: today.toISOString(),
+    };
+
+    // Steps
+    const steps = await readRecords('Steps', { timeRangeFilter });
+    const totalSteps = steps.reduce((sum, cur) => sum + cur.count, 0);
+    setSteps(totalSteps);
+  };
+
+  getHealthData();
+}, [androidPermissions]);
+
+
 
   return (
     <View style={styles.container}>
@@ -84,7 +175,8 @@ useEffect(() => {
  
   <Value label = "Steps" value = {steps.toString()} /> 
   <Value label = "Sleep" value = {sleep.toString()} />
-  <Value label = "Distance" value = {`${distance.toString()} m `}/>
+  <Value label = "Walking + Running Distance" value = {`${distance.toString()} m `}/>
+  <Value label = "Active Energy" value = {`${active.toString()} cal `}/>
 
       <StatusBar style="auto" />
     </View>
